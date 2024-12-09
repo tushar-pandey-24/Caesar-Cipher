ca94a610695b94094f680b842379c8eb83ff1049
Tushar Pandey <tushar.pandey.off@gmail.com>
2024-12-09 22:01:30 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+# Caesar Cipher
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
+
+
+def caesar_cipher(user_txt, direct, shift):
+    shift %= 26
+
+    if direct == "decode":
+        shift *= -1
+
+    cipher_text = ""
+
+    for letter in user_txt:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        else:
+            cipher_text += letter
+
+    print(f"The {direct}ed message is {cipher_text}.")
+
+
+while True:
+    direction = input("Type 'encode' to encrypt or 'decode' to decrypt: ").lower()
+    user_input = input("Enter the message: ").lower()
+    shift_amount = int(input("What's the shift amount?: "))
+
+    caesar_cipher(user_txt=user_input, direct=direction, shift=shift_amount)
+
+    if input("Do you want to continue?: ").lower() == "no":
+        break
+        
